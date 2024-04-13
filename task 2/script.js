614c4fccc8c40f2b9d1d936ebfd416ad7881de3c
Valerija Schulz <135040463+zzzVSchulzzz@users.noreply.github.com>
2024-04-13 08:24:41 +0300

    Create script.js

---
+++ task 2/script.js
@@ -0,0 +1,10 @@
+/*
+Напишите функцию getPercents(percent, number), 
+которая возвращает {percent} процентов от {number}.
+Пояснение: например, getPercents(30, 200) должно вернуть 60.
+*/
+
+function getPercents(percent, number){
+    return percent * 100 / number
+}
+getPercents(7, 70);
